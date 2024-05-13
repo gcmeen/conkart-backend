0cff89877a43f0ba965395117ee54135af9931cb
chetan godhani <cdgodhani@gmail.com>
2024-05-13 16:58:50 +0530

    updates

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,7 +1,7 @@
 const authJwt = require("./authJwt");
-const verifySignUp = require("./verifySignUp");
+const validations = require("./validations");
 
 module.exports = {
   authJwt,
-  verifySignUp
+  validations
 };
--- server.js
+++ server.js
@@ -2,18 +2,24 @@ const express = require("express");
 const cors = require("cors");
 const cookieSession = require("cookie-session");
 
-const dbConfig = require("./app/config/db.config");
+const config = require('./config/config');
+const appConfig = require('./lib/app.config');
+const indexRoutes = require('./routes')
+const db = require("./lib/db.connection");
+(async () => { await db.connectDb() })();
 
+//initialize cron job
+const CronJob = require('./cron_jobs/index')
 const app = express();
 
-app.use(cors());
+// app.use(cors());
 /* for Angular Client (withCredentials) */
-// app.use(
-//   cors({
-//     credentials: true,
-//     origin: ["http://localhost:8081"],
-//   })
-// );
+app.use(
+  cors({
+    credentials: true,
+    origin: config.corsAllowOrigins,
+  })
+);
 
 // parse requests of content-type - application/json
 app.use(express.json());
@@ -23,76 +29,24 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cookieSession({
-    name: "bezkoder-session",
-    keys: ["COOKIE_SECRET"], // should use as secret environment variable
-    httpOnly: true
+    name: "conkart-session",
+    keys: [config.cookieSessionSecret], // should use as secret environment variable
+    // httpOnly: true
   })
 );
 
-const db = require("./app/models");
-const Role = db.role;
+// handle api params and details
+app.use(appConfig.trimParams);
 
-db.mongoose
-  .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(() => {
-    console.log("Successfully connect to MongoDB.");
-    initial();
-  })
-  .catch(err => {
-    console.error("Connection error", err);
-    process.exit();
-  });
+// V1  api Routes
+app.use('/api/v1',indexRoutes);
 
-// simple route
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
-});
-
-// routes
-require("./app/routes/auth.routes")(app);
-require("./app/routes/user.routes")(app);
+// handle api errors 
+app.use(appConfig.handleError);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = config.port;
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
-
-function initial() {
-  Role.estimatedDocumentCount((err, count) => {
-    if (!err && count === 0) {
-      new Role({
-        name: "user"
-      }).save(err => {
-        if (err) {
-          console.log("error", err);
-        }
-
-        console.log("added 'user' to roles collection");
-      });
-
-      new Role({
-        name: "moderator"
-      }).save(err => {
-        if (err) {
-          console.log("error", err);
-        }
-
-        console.log("added 'moderator' to roles collection");
-      });
-
-      new Role({
-        name: "admin"
-      }).save(err => {
-        if (err) {
-          console.log("error", err);
-        }
-
-        console.log("added 'admin' to roles collection");
-      });
-    }
-  });
-}
